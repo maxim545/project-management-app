fa215ec2ab64893580c3816a2447231e1aca4735
maxim545 <maxximc95@gmail.com>
2022-10-30 18:05:49 +0300

    feat: generate necessary for project components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { EditProfileComponent } from './pages/edit-profile/edit-profile.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    SignUpComponent,
+    EditProfileComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './pages/main/main.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainComponent,
+    WelcomeComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
