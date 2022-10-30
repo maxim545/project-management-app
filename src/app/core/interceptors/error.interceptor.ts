3c6da6db9baa24e96b301883d5a96ef9ad2326f3
maxim545 <maxximc95@gmail.com>
2022-10-30 21:31:38 +0300

    feat: add interceptors

---
+++ src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+import { ErrorHandlerService } from '../services/error-handler/error-handler.service';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(
+    private errorHndler: ErrorHandlerService,
+  ) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request)
+      .pipe(
+        catchError((err: HttpErrorResponse) => {
+          const errorMessage = `status: ${err.status}\n  ERROR message: ${err.message}`;
+          return throwError(() => errorMessage);
+        }),
+      );
+  }
+}
